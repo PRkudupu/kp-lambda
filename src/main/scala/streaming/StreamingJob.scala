755c606cacb2402c5b0404a1cdcb8fe90d9f317c
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2020-05-17 23:54:16 -0700

    data loaded for streaming.

--- src/main/scala/streaming/StreamingJob.scala
+++ src/main/scala/streaming/StreamingJob.scala
@@ -1,11 +1,14 @@
 package batch
 package clickstream
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.catalyst.ScalaReflection
+import domain.Activity
 
 object StreamingJob {
   def main(args: Array[String]): Unit = {
     //location for data source
-    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\data.tsv"
+    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\input"
 
     //Create a spark session specify the app name
     val spark = SparkSession
@@ -17,14 +20,17 @@ object StreamingJob {
     //set spark log level to only error
     spark.sparkContext.setLogLevel("ERROR")
 
+    //convert case class into schema
+    val schema = ScalaReflection.schemaFor[Activity].dataType.asInstanceOf[StructType]
+
     // For implicit conversions from RDDs to DataFrames
 
     val MS_IN_HOUR = 1000 * 60 * 60
     // Create an RDD of Person objects from a text file, convert it to a Dataframe
     val inputDF = spark
       .readStream
+      .schema(schema)
       .load(sourceFile)
-
     println(inputDF)
 
 
