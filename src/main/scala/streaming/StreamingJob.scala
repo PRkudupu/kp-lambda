1a5d8b65804bd53286b072389d29e9f7e130faf6
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2020-05-18 01:37:58 -0700

    udf function has been added.

--- src/main/scala/streaming/StreamingJob.scala
+++ src/main/scala/streaming/StreamingJob.scala
@@ -2,9 +2,9 @@ package batch
 package clickstream
 import domain.ActivityStreams
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.StructType
 
 object StreamingJob {
   def main(args: Array[String]): Unit = {
@@ -33,9 +33,24 @@ object StreamingJob {
          .option("delimiter","\t")
          .csv(sourceFile)
 
+    def convertTime=(s:Long)=>{
+      s/MS_IN_HOUR * MS_IN_HOUR
+    }
+    spark.udf.register("convertTimeUDF",convertTime)
+
+    inputDF.createOrReplaceTempView("activity")
+    val activityByProduct = spark.sql("""SELECT
+                                            product,
+                                            convertTimeUDF(timestamp_hour) timestamp_hour,
+                                            sum(case when action = 'purchase' then 1 else 0 end) as purchase_count,
+                                            sum(case when action = 'add_to_cart' then 1 else 0 end) as add_to_cart_count,
+                                            sum(case when action = 'page_view' then 1 else 0 end) as page_view_count
+                                            from activity
+                                            group by product, timestamp_hour """)
+
     //Write
-    inputDF.writeStream
-      .outputMode("append")
+    activityByProduct.writeStream
+      .outputMode("complete")
       .format("console")
       .option("truncate","false")
       .trigger(Trigger.ProcessingTime("2 seconds"))
