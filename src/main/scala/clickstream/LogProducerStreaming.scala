a6010c16f7bddda77a604378b311521059686434
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2020-05-17 23:41:46 -0700

    seperate object creating for generating batch data.

--- src/main/scala/clickstream/LogProducerStreaming.scala
+++ src/main/scala/clickstream/LogProducerStreaming.scala
@@ -8,7 +8,7 @@ import org.apache.commons.io.FileUtils
 import scala.util.Random
 
 
-object LogProducer extends App {
+object LogProducer_streaming extends App {
   // WebLog config
   val wlc = Settings.WebLogGen
 
